fb82bd24c081ff7f37e64717c2da4319c3a4e769
Spencer Rak <spencer.rak@gmail.com>
2025-02-07 15:45:43 +0000

    Use invoke

---
+++ tasks.py
@@ -0,0 +1,65 @@
+import json
+
+from invoke import task, call
+from pathlib import Path
+from hashlib import sha256
+
+fp_conf_hash = Path("oci/nginx/.nginx.conf.sha256")
+d_content = None
+
+@task
+def nginx_vol(c):
+    global d_content
+    r = c.run("podman volume inspect content", warn=True)
+    if r.exited not in (0, 125):
+        breakpoint()
+    if r.exited == 0:
+        vol = json.loads(r.stdout)
+        if vol:
+            vol = vol[0]
+            d_content = vol["Mountpoint"]
+            return
+    c.run("podman volume create content")
+
+@task
+def nginx_img(c):
+    conf_hash = None
+    if fp_conf_hash.is_file():
+        fp_conf_hash.read_text()
+    cur_conf_hash = sha256(Path("oci/nginx/nginx.conf").read_text())
+    if cur_conf_hash == conf_hash:
+        return
+    fp_conf_hash.write_text(cur_conf_hash)
+    print("Regenerating nginx")
+    c.run("podman build -t proxy:latest -f oci/nginx/Containerfile")
+
+@task(pre=[nginx_vol], aliases=["content"])
+def nginx_content(c):
+    c.run(f"rsync --no-compress -a -d static/ {d_content}")
+
+@task(pre=[nginx_img, nginx_content], aliases=["proxy"])
+def nginx(c):
+    c.run("podman run -d --rm --name nginx "
+        "-v ./nginx.conf:/etc/nginx/nginx.conf:ro,Z "
+        "-v content:/usr/share/nginx/html:ro,Z "
+        "-v certs:/etc/ssl/subatomic:ro,Z "
+        "--network=host "
+        "docker.io/library/nginx:latest",
+        warn=True
+    )
+
+@task(post=["nginx"])
+def restart(c):
+    c.run("podman stop nginx", warn=True)
+
+@task
+def server(c):
+    c.run("uvicorn --reload main:app", pty=True)
+
+@task
+def postgres(c):
+    c.run("""podman run -d --rm --name test 
+-e "POSTGRES_ADMIN=admin" 
+-e "POSTGRES_PASSWORD=admin" 
+-e "POSTGRES_DATABASE=test" 
+docker.io/library/postgres:latest""")
