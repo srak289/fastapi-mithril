7e730b826ee13f41c5e078b0d89aab92b6be62ad
Spencer Rak <spencer.rak@gmail.com>
2025-02-08 16:33:21 +0000

    Adds css. Inherits components

--- static/js/main.js
+++ static/js/main.js
@@ -2,12 +2,42 @@ import 'mithril'
 
 let root = document.body
 
-let RootComponent = {
-    view: function() {
-        return m("h1", {class: "title"}, "Test")
+const pages = {root: "/", users: "/users", login: "/login"}
+
+let LinksComponent = {
+    view: function(vnode) {
+        const pages = {root: "/", users: "/users", login: "/login"}
+        return m("div", {class: "main"}, [
+            m("div", {class: "links-bar"}, Object.entries(pages).map(([k, v]) =>
+                m("div", {class: "links-item"},
+                    m(m.route.Link, {href: v}, k)
+                )
+            )),
+            vnode.children
+        ]);
     }
 }
 
+let RootComponent = {
+    view: function() {
+        return m(LinksComponent, [
+            m("h1", {class: "root-component"}, "Test"),
+            m("p", {class: "root-component-content"}, "This is content"),
+        ]);
+    }
+}
+
+let UsersComponent = {
+    view: function() {
+        return m(LinksComponent, {class: "users"}, function() {
+            m("h1", {class: "title"}, "Test")
+        })
+    }
+}
+
+
 m.route(root, "/", {
     "/": RootComponent,
+    "/users": LinksComponent,
+    "/login": LinksComponent,
 })
